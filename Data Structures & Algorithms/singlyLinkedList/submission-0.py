977914c23333dcf20fe4f3147d86628833023c6e
Ramia Ambreen <143974522+Ramia1205@users.noreply.github.com>
2026-09-02 23:09:16 -0400

    Add: singlyLinkedList - submission-0

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-0.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+
+    
+    def get(self, index: int) -> int:
+        curr = self.head
+        
+        for _ in range(index):
+            if not curr:
+                return -1
+            curr = curr.next
+        
+        if not curr:
+            return -1
+        
+        return curr.val
+
+    def insertHead(self, val: int) -> None:
+        node = Node(val)
+        node.next = self.head
+        self.head = node
+
+    def insertTail(self, val: int) -> None:
+        node = Node(val)
+        
+        if not self.head:
+            self.head = node
+            return
+        
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        
+        curr.next = node
+
+    def remove(self, index: int) -> bool:
+        if not self.head:
+            return False
+        
+        if index == 0:
+            self.head = self.head.next
+            return True
+        
+        curr = self.head
+        
+        for _ in range(index - 1):
+            if not curr or not curr.next:
+                return False
+            curr = curr.next
+        
+        if not curr or not curr.next:
+            return False
+        
+        curr.next = curr.next.next
+        return True
+
+    def getValues(self) -> List[int]:
+        values = []
+        curr = self.head
+        
+        while curr:
+            values.append(curr.val)
+            curr = curr.next
+        
+        return values
